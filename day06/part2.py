fb88bfa1d3c75b4125fb6b984da2bc4f0588cba3
Skye Berghel <skye.berghel@tmlt.io>
2022-12-21 15:58:25 -0800

    day 6

---
+++ day06/part2.py
@@ -0,0 +1,29 @@
+import argparse
+from typing import List
+
+
+def find_marker(s: str) -> int:
+    prev_chars: List[str] = []
+    for i, e in enumerate(s):
+        if len(prev_chars) < 14:
+            prev_chars.append(e)
+            continue
+        prev_chars.append(e)
+        prev_chars = prev_chars[1:]
+        if len(set(prev_chars)) == 14:
+            return i + 1
+    raise ValueError("no unique set of 14 consecutive characters in string")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="read a file")
+    parser.add_argument("filename")  # positional argument
+
+    args = parser.parse_args()
+    print("case 0 (should be 19):", find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+    print("case 1 (should be 23):", find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"))
+    print("case 2 (should be 23):", find_marker("nppdvjthqldpwncqszvftbrmjlhg"))
+    print()
+    with open(args.filename) as f:
+        for line in f:
+            print("from file:", find_marker(line))
